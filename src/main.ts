1f9d33ea18e84959ca42a4846186f1aa4c8504d3
Gian Avila <avila.gian111@gmail.com>
2026-03-02 16:55:51 +0700

    feat: add cookie-parser middleware and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
-import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const httpAdapterHost = app.get(HttpAdapterHost);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
+
+  const allowedOrigins = [process.env.API_URL, process.env.CMS_URL];
+
+  app.use(cookieParser());
+  app.enableCors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
